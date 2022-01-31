fc886e9ef6f99b3c438df3912aefcb2352fb10b1
Pavan V <80445245+PavanV3@users.noreply.github.com>
2022-01-31 22:00:13 +0530

    Add files via upload

---
+++ Phase_2/Phase_2_Ass.Practices/src/phase2_Servlets/PostHandler.java
@@ -0,0 +1,32 @@
+package phase2_Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/PostHandler")
+public class PostHandler extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		   PrintWriter out = response.getWriter();
+           out.println("<html><body>");
+           
+           String name = request.getParameter("name");
+           String address = request.getParameter("address");
+           
+           out.println("Name=" + name + "<br>Address=" + address);
+           
+           out.println("</body></html>");
+	}
+
+}
