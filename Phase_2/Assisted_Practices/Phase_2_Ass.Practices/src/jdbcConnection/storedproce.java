1e741f71d0e0211cdd4296e024d70dee50bba731
Pavan V <80445245+PavanV3@users.noreply.github.com>
2022-01-31 21:41:40 +0530

    Add files via upload

---
+++ Phase_2/Assisted_Practices/Phase_2_Ass.Practices/src/jdbcConnection/storedproce.java
@@ -0,0 +1,28 @@
+package jdbcConnection;
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+
+	public class storedproce {
+
+	        private Connection connection;
+	        
+	        public storedproce(String dbURL, String user, String pwd) throws ClassNotFoundException, SQLException
+	        {
+	                
+	                Class.forName("com.mysql.jdbc.Driver");
+	                this.connection = DriverManager.getConnection(dbURL, user, pwd);
+	        }
+	        
+	        public Connection getConnection()
+	        {
+	                return this.connection;
+	        }
+	        
+	        public void closeConnection() throws SQLException 
+	        {
+	                if (this.connection != null)
+	                        this.connection.close();
+	        }
+	}
+
